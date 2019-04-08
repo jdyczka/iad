c0eccfbcf6c69d5cf5982a2add3856298012c626
Joanna Dyczka <asia.dyczka@gmail.com>
2019-04-08 16:01:41 +0200

    delete unused code

--- irysy.py
+++ irysy.py
@@ -4,7 +4,6 @@ testFilePath = 'assets/data_test.csv'
 import csv
 import math
 import operator
-# from heapq import nsmallest
 from collections import Counter 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,11 +13,8 @@ from pandas.plotting import table
 def main():
     trainData = list(csv.reader(open(trainFilePath), quoting=csv.QUOTE_NONNUMERIC))
     testData = list(csv.reader(open(testFilePath), quoting=csv.QUOTE_NONNUMERIC))
-    # trainDataSmall = [[5.1,3.5,1.4,0.2,0]]
-    # testDataSmall = [[4.9,3.0,1.4,0.2,0]]
 
     kValues = [1, 3, 5, 7, 9, 11]
-    # kValues = [3]
 
     handleCase(trainData, testData, kValues, [0,1,2,3])
 
@@ -99,23 +95,6 @@ def plotConfusionMatrix(errors, counts, propStr):
     print('\n----------------------------------------\n' + title + '\n')
     print(df)
 
-    # fig, ax = plt.subplots()
-
-    # # hide axes
-    # # fig.patch.set_visible(False)
-    # # ax.axis('off')
-    # # ax.axis('tight')
-
-    # ax.set_xlabel('wartość przewidywana')
-    # ax.set_ylim(-1,0)
-    # # ax.yaxis.ticks(False)
-
-    # ax.table(cellText=df.values, rowLabels=df.index, colLabels=df.columns, loc='center')
-
-    # fig.tight_layout()
-
-    # plt.savefig('charts/matrix' + propStr + '.png')
-
 
 def processAndReturnErrors(trainData, testData, kValues, properties):
     
@@ -123,7 +102,6 @@ def processAndReturnErrors(trainData, testData, kValues, properties):
     for k in kValues:
         currentErrors = []
         for testCase in testData:
-            # distances = []
             neighbours = []
             for trainCase in trainData:
                 distance = 0
@@ -131,15 +109,12 @@ def processAndReturnErrors(trainData, testData, kValues, properties):
                     distance += (testCase[p] - trainCase[p])**2
 
                 distance = math.sqrt(distance)
-                # distances.append(distance)
                 if len(neighbours) < k: 
                     neighbours.append([distance, trainCase[4]])
                 else:
                     neighbours = sorted(neighbours, key=operator.itemgetter(0)) 
                     if distance < neighbours[k-1][0]:
                         neighbours[k-1] = [distance, trainCase[4]]
-            # smallestDistances = nsmallest(k, distances)
-            # print(smallestDistances)
             neighboursSpecies = list(map(lambda n: n[1], neighbours))
             occurence_count = Counter(neighboursSpecies) 
             species = occurence_count.most_common(1)[0][0]
